125a51940927892a692b18e5ea3519a5c9bcab32
Manon Heine <30656550+manonheine@users.noreply.github.com>
2019-06-07 11:49:05 +0200

    Section 11 L133: Try JS

---
+++ S11L133/try.js
@@ -0,0 +1,13 @@
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// numbers.forEach(function(num) {
+//   if (num % 3 === 0) {
+//   console.log (num);
+//   }
+// })
+
+for (var numbers = 1; numbers <= 10; numbers++) {
+  if (numbers % 3 === 0) {
+    console.log (numbers);
+  }
+}
